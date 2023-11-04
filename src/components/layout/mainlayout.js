cc75cbbd162a30869c014e6c77a6059ad9ddbc01
Ogheneochuko Pedro <pedroogheneochuko@gmail.com>
2023-11-04 19:29:38 +0100

    Fix image credit positioning

--- src/components/layout/mainlayout.js
+++ src/components/layout/mainlayout.js
@@ -1,11 +1,17 @@
 import Footer from "./footer";
 import Navbar from "./navbar";
 
-const MainLayout = ({ children }) => {
+const MainLayout = ({ children, childrenSectionRelative }) => {
   return (
     <div className="flex flex-col min-h-screen w-full">
       <Navbar />
-      <main className="flex-grow px-3 mb-5">{children}</main>
+      <main
+        className={`flex-grow px-3 mb-5${
+          childrenSectionRelative ? " relative" : ""
+        }`}
+      >
+        {children}
+      </main>
       <Footer />
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,8 +21,24 @@ export default function Home() {
           href="/favicon.ico"
         />
       </Head>
-      <MainLayout>
+      <MainLayout childrenSectionRelative>
         <div className="w-full">
+          <p className="absolute bottom-0 left-5 py-1 px-2 bg-black/40 rounded-full">
+            Photo by{" "}
+            <a
+              href="https://unsplash.com/@flyd2069?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+              className="text-primary"
+            >
+              FLY:D
+            </a>{" "}
+            on{" "}
+            <a
+              href="https://unsplash.com/photos/zAhAUSdRLJ8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+              className="text-primary"
+            >
+              Unsplash
+            </a>
+          </p>
           <div className="flex flex-col items-center justify-center h-full max-w-3xl mx-auto">
             <SendPackage />
             <h1 className="text-6xl font-bold mt-20 text-primary">
@@ -36,36 +52,20 @@ export default function Home() {
               &nbsp; data to your teammates securely.
             </p>
             <Link href="/send">
-              <button className="mt-20 text-xl rounded px-3 py-2 bg-primary text-white">
+              <button className="my-20 text-xl rounded px-3 py-2 bg-primary text-white">
                 Get started
               </button>
             </Link>
           </div>
         </div>
         <div
-          className="w-full absolute top-0 left-0 h-screen bg-cover bg-center bg-no-repeat z-[-1]"
+          className="w-full fixed top-0 left-0 h-screen bg-cover bg-center bg-no-repeat z-[-1]"
           style={{
             backgroundImage:
               "url(https://assets.ochuko.space/static/securesend_thumbnail-W1nbOA.webp)",
           }}
         ></div>
-        <div className="w-full absolute top-0 left-0 h-screen bg-black/50 z-[-1]"></div>
-        <p className="fixed bottom-20 left-5 py-1 px-2 bg-black/40 rounded-full">
-          Photo by{" "}
-          <a
-            href="https://unsplash.com/@flyd2069?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-            className="text-primary"
-          >
-            FLY:D
-          </a>{" "}
-          on{" "}
-          <a
-            href="https://unsplash.com/photos/zAhAUSdRLJ8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-            className="text-primary"
-          >
-            Unsplash
-          </a>
-        </p>
+        <div className="w-full fixed top-0 left-0 h-screen bg-black/50 z-[-1]"></div>
       </MainLayout>
     </>
   );
