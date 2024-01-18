1ab8b3b24872d528c26f8c6b5b6d3cf46f4ff3a6
Ochuko Pedro <ochuko.space@gmail.com>
2024-01-18 09:25:09 +0100

    feat: cleanup before publication (#10)
    * feat: sendfort revamp
    
    * chore: remove unused assets
    
    * feat: set up cors
    
    * feat: add vercel analytics
    
    * feat: add seo

--- src/pages/api/content/[slug].js
+++ src/pages/api/content/[slug].js
@@ -1,16 +1,9 @@
 import { Content } from "@/models";
 import connectDB from "@/utils/connectDB";
 import { decryptString } from "@/utils/encryptObject";
-import runMiddleware from "@/utils/middleware";
-import Cors from "cors";
-
-const cors = Cors({
-  origin: "*",
-  methods: ["GET"],
-});
 
 export default async function contentAPI(req, res) {
-  await runMiddleware(req, res, cors);
+  await connectDB();
 
   await connectDB();
 
